1b0f99e4e92a0ba96180c676e97fe9e626252472
Ryan Bretschneider <ryan.bretschneider@maine.edu>
2012-02-12 17:06:47 -0500

    Adding files?

---
+++ CalculatorProjectOne/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace CalculatorProjectOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnseven = new System.Windows.Forms.Button();
+            this.btnsix = new System.Windows.Forms.Button();
+            this.btnthree = new System.Windows.Forms.Button();
+            this.btnnine = new System.Windows.Forms.Button();
+            this.btneight = new System.Windows.Forms.Button();
+            this.btnfive = new System.Windows.Forms.Button();
+            this.btntwo = new System.Windows.Forms.Button();
+            this.btnfour = new System.Windows.Forms.Button();
+            this.btnone = new System.Windows.Forms.Button();
+            this.btnzero = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsubtract = new System.Windows.Forms.Button();
+            this.btnmultiply = new System.Windows.Forms.Button();
+            this.btndivide = new System.Windows.Forms.Button();
+            this.btnequals = new System.Windows.Forms.Button();
+            this.activebox = new System.Windows.Forms.TextBox();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnseven
+            // 
+            this.btnseven.Location = new System.Drawing.Point(24, 64);
+            this.btnseven.Name = "btnseven";
+            this.btnseven.Size = new System.Drawing.Size(75, 23);
+            this.btnseven.TabIndex = 0;
+            this.btnseven.Text = "7";
+            this.btnseven.UseVisualStyleBackColor = true;
+            this.btnseven.Click += new System.EventHandler(this.btnseven_Click);
+            // 
+            // btnsix
+            // 
+            this.btnsix.Location = new System.Drawing.Point(186, 92);
+            this.btnsix.Name = "btnsix";
+            this.btnsix.Size = new System.Drawing.Size(75, 22);
+            this.btnsix.TabIndex = 1;
+            this.btnsix.Text = "6";
+            this.btnsix.UseVisualStyleBackColor = true;
+            this.btnsix.Click += new System.EventHandler(this.btnsix_Click);
+            // 
+            // btnthree
+            // 
+            this.btnthree.Location = new System.Drawing.Point(186, 122);
+            this.btnthree.Name = "btnthree";
+            this.btnthree.Size = new System.Drawing.Size(75, 23);
+            this.btnthree.TabIndex = 2;
+            this.btnthree.Text = "3";
+            this.btnthree.UseVisualStyleBackColor = true;
+            this.btnthree.Click += new System.EventHandler(this.btnthree_Click);
+            // 
+            // btnnine
+            // 
+            this.btnnine.Location = new System.Drawing.Point(186, 64);
+            this.btnnine.Name = "btnnine";
+            this.btnnine.Size = new System.Drawing.Size(75, 23);
+            this.btnnine.TabIndex = 3;
+            this.btnnine.Text = "9";
+            this.btnnine.UseVisualStyleBackColor = true;
+            this.btnnine.Click += new System.EventHandler(this.btnnine_Click);
+            // 
+            // btneight
+            // 
+            this.btneight.Location = new System.Drawing.Point(105, 64);
+            this.btneight.Name = "btneight";
+            this.btneight.Size = new System.Drawing.Size(75, 23);
+            this.btneight.TabIndex = 4;
+            this.btneight.Text = "8";
+            this.btneight.UseVisualStyleBackColor = true;
+            this.btneight.Click += new System.EventHandler(this.btneight_Click);
+            // 
+            // btnfive
+            // 
+            this.btnfive.Location = new System.Drawing.Point(105, 93);
+            this.btnfive.Name = "btnfive";
+            this.btnfive.Size = new System.Drawing.Size(75, 23);
+            this.btnfive.TabIndex = 5;
+            this.btnfive.Text = "5";
+            this.btnfive.UseVisualStyleBackColor = true;
+            this.btnfive.Click += new System.EventHandler(this.btnfive_Click);
+            // 
+            // btntwo
+            // 
+            this.btntwo.Location = new System.Drawing.Point(105, 122);
+            this.btntwo.Name = "btntwo";
+            this.btntwo.Size = new System.Drawing.Size(75, 23);
+            this.btntwo.TabIndex = 6;
+            this.btntwo.Text = "2";
+            this.btntwo.UseVisualStyleBackColor = true;
+            this.btntwo.Click += new System.EventHandler(this.btntwo_Click);
+            // 
+            // btnfour
+            // 
+            this.btnfour.Location = new System.Drawing.Point(24, 93);
+            this.btnfour.Name = "btnfour";
+            this.btnfour.Size = new System.Drawing.Size(75, 23);
+            this.btnfour.TabIndex = 7;
+            this.btnfour.Text = "4";
+            this.btnfour.UseVisualStyleBackColor = true;
+            this.btnfour.Click += new System.EventHandler(this.btnfour_Click);
+            // 
+            // btnone
+            // 
+            this.btnone.Location = new System.Drawing.Point(24, 122);
+            this.btnone.Name = "btnone";
+            this.btnone.Size = new System.Drawing.Size(75, 23);
+            this.btnone.TabIndex = 8;
+            this.btnone.Text = "1";
+            this.btnone.UseVisualStyleBackColor = true;
+            this.btnone.Click += new System.EventHandler(this.btnone_Click);
+            // 
+            // btnzero
+            // 
+            this.btnzero.Location = new System.Drawing.Point(105, 151);
+            this.btnzero.Name = "btnzero";
+            this.btnzero.Size = new System.Drawing.Size(75, 23);
+            this.btnzero.TabIndex = 9;
+            this.btnzero.Text = "0";
+            this.btnzero.UseVisualStyleBackColor = true;
+            this.btnzero.Click += new System.EventHandler(this.btnzero_Click);
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(319, 64);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 10;
+            this.btnadd.Text = "+";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnsubtract
+            // 
+            this.btnsubtract.Location = new System.Drawing.Point(319, 92);
+            this.btnsubtract.Name = "btnsubtract";
+            this.btnsubtract.Size = new System.Drawing.Size(75, 23);
+            this.btnsubtract.TabIndex = 11;
+            this.btnsubtract.Text = "-";
+            this.btnsubtract.UseVisualStyleBackColor = true;
+            this.btnsubtract.Click += new System.EventHandler(this.btnsubtract_Click);
+            // 
+            // btnmultiply
+            // 
+            this.btnmultiply.Location = new System.Drawing.Point(319, 121);
+            this.btnmultiply.Name = "btnmultiply";
+            this.btnmultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnmultiply.TabIndex = 12;
+            this.btnmultiply.Text = "*";
+            this.btnmultiply.UseVisualStyleBackColor = true;
+            this.btnmultiply.Click += new System.EventHandler(this.btnmultiply_Click);
+            // 
+            // btndivide
+            // 
+            this.btndivide.Location = new System.Drawing.Point(319, 150);
+            this.btndivide.Name = "btndivide";
+            this.btndivide.Size = new System.Drawing.Size(75, 23);
+            this.btndivide.TabIndex = 13;
+            this.btndivide.Text = "/";
+            this.btndivide.UseVisualStyleBackColor = true;
+            this.btndivide.Click += new System.EventHandler(this.btndivide_Click);
+            // 
+            // btnequals
+            // 
+            this.btnequals.Location = new System.Drawing.Point(319, 186);
+            this.btnequals.Name = "btnequals";
+            this.btnequals.Size = new System.Drawing.Size(75, 23);
+            this.btnequals.TabIndex = 14;
+            this.btnequals.Text = "=";
+            this.btnequals.UseVisualStyleBackColor = true;
+            this.btnequals.Click += new System.EventHandler(this.btnequals_Click);
+            // 
+            // activebox
+            // 
+            this.activebox.Location = new System.Drawing.Point(12, 12);
+            this.activebox.Name = "activebox";
+            this.activebox.Size = new System.Drawing.Size(301, 20);
+            this.activebox.TabIndex = 15;
+            this.activebox.Text = "0.0";
+            this.activebox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(319, 12);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 16;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 221);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.activebox);
+            this.Controls.Add(this.btnequals);
+            this.Controls.Add(this.btndivide);
+            this.Controls.Add(this.btnmultiply);
+            this.Controls.Add(this.btnsubtract);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.btnzero);
+            this.Controls.Add(this.btnone);
+            this.Controls.Add(this.btnfour);
+            this.Controls.Add(this.btntwo);
+            this.Controls.Add(this.btnfive);
+            this.Controls.Add(this.btneight);
+            this.Controls.Add(this.btnnine);
+            this.Controls.Add(this.btnthree);
+            this.Controls.Add(this.btnsix);
+            this.Controls.Add(this.btnseven);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnseven;
+        private System.Windows.Forms.Button btnsix;
+        private System.Windows.Forms.Button btnthree;
+        private System.Windows.Forms.Button btnnine;
+        private System.Windows.Forms.Button btneight;
+        private System.Windows.Forms.Button btnfive;
+        private System.Windows.Forms.Button btntwo;
+        private System.Windows.Forms.Button btnfour;
+        private System.Windows.Forms.Button btnone;
+        private System.Windows.Forms.Button btnzero;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnsubtract;
+        private System.Windows.Forms.Button btnmultiply;
+        private System.Windows.Forms.Button btndivide;
+        private System.Windows.Forms.Button btnequals;
+        private System.Windows.Forms.TextBox activebox;
+        private System.Windows.Forms.Button btnclear;
+    }
+}
+
---
+++ CalculatorProjectOne/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculatorProjectOne
+{
+   
+
+    public partial class Form1 : Form
+    { 
+        private double activenum = 0;
+        private double activetemp = 0;
+       // private double currentans = 0;
+        public Boolean operate = false;
+        private int operation = 0;
+        String temp = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnseven_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "7";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "7";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btneight_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "8";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "8";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btnnine_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "9";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "9";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btnfour_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "4";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "4";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btnfive_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "5";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "5";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btnsix_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "6";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "6";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btnone_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "1";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "1";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btntwo_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "2";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "2";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btnthree_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "3";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "3";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btnzero_Click(object sender, EventArgs e)
+        {
+            if (operate == false)
+            {
+                temp = activenum.ToString();
+                temp = temp + "0";
+                activenum = Convert.ToDouble(temp);
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                temp = activetemp.ToString();
+                temp = temp + "0";
+                activetemp = Convert.ToDouble(temp);
+                activebox.Text = activetemp.ToString();
+            }
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            if (operate == true)
+            {
+                double ans = 0;
+                if (operation == 1)
+                {
+                    ans = activenum + activetemp;
+                }
+                else if (operation == 2)
+                {
+                    ans = activenum - activetemp;
+                }
+                else if (operation == 3)
+                {
+                    ans = activenum * activetemp;
+                }
+                else if (operation == 4)
+                {
+                    ans = activenum / activetemp;
+                }
+                activenum = ans;
+                activetemp = 0;
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                operate = true;
+               
+            }
+            operation = 1;
+        }
+
+        private void btnsubtract_Click(object sender, EventArgs e)
+        {
+            if (operate == true) {
+                double ans = 0;
+                if (operation == 1)
+                {
+                    ans = activenum + activetemp;
+                }
+                else if (operation == 2)
+                {
+                    ans = activenum - activetemp;
+                }
+                else if (operation == 3)
+                {
+                    ans = activenum * activetemp;
+                }
+                else if (operation == 4)
+                {
+                    ans = activenum / activetemp;
+                }
+                activenum = ans;
+                activetemp = 0;
+                activebox.Text = activenum.ToString();
+            }
+            else {
+
+            operate = true;
+            
+            }
+            operation = 2;
+        }
+
+        private void btnmultiply_Click(object sender, EventArgs e)
+        {
+            if (operate == true)
+            {
+                double ans = 0;
+                if (operation == 1)
+                {
+                    ans = activenum + activetemp;
+                }
+                else if (operation == 2)
+                {
+                    ans = activenum - activetemp;
+                }
+                else if (operation == 3)
+                {
+                    ans = activenum * activetemp;
+                }
+                else if (operation == 4)
+                {
+                    ans = activenum / activetemp;
+                }
+                activenum = ans;
+                activetemp = 0;
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                operate = true;
+                
+            }
+            operation = 3;
+        }
+
+        private void btndivide_Click(object sender, EventArgs e)
+        {
+            if (operate == true)
+            {
+                double ans = 0;
+                if (operation == 1)
+                {
+                    ans = activenum + activetemp;
+                }
+                else if (operation == 2)
+                {
+                    ans = activenum - activetemp;
+                }
+                else if (operation == 3)
+                {
+                    ans = activenum * activetemp;
+                }
+                else if (operation == 4)
+                {
+                    ans = activenum / activetemp;
+                }
+                activenum = ans;
+                activetemp = 0;
+                activebox.Text = activenum.ToString();
+            }
+            else
+            {
+                operate = true;
+                
+            }
+            operation = 4;
+        }
+
+        private void btnequals_Click(object sender, EventArgs e)
+        {
+            //do the correct operation
+            double ans = 0;
+            if (operation == 1){
+            ans = activenum + activetemp;
+        }
+            else if(operation ==2){
+                ans = activenum - activetemp;
+            }
+            else if (operation == 3)
+            {
+                ans = activenum * activetemp;
+            }
+            else if (operation == 4)
+            {
+                ans = activenum / activetemp;
+            }
+            if (operate == false)
+            {
+            }
+            activenum = ans;
+            //currentans = ans;
+            activebox.Text = activenum.ToString();
+            ans = 0;
+            operate = false;
+            operation = 0;
+            activetemp = 0;
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            activenum = 0;
+            activetemp = 0;
+            operate = false;
+            operation = 0;
+            activebox.Text = "0.0";
+        }
+
+    }
+}
